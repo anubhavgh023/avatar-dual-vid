82b75543ee65d9760702f2d57d39375686406b5f
Anubhav <anubhavsamanta023@gmail.com>
2025-03-31 01:49:09 +0530

    feat: middleware added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 import os
@@ -11,6 +12,19 @@ from helpers.aws_uploader import upload_to_s3
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:*",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Define the request body model
 class VideoRequest(BaseModel):
